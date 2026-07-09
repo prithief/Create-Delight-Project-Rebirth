f98e719ae2d22abd93883be8af8144fec7276db2
JIA <430731352@qq.com>
2026-07-09 19:25:40 +0800

    feat: 同步createadditionallogistics相关魔改

--- kubejs/server_scripts/mods/createadditionallogistics/recipes.js
+++ kubejs/server_scripts/mods/createadditionallogistics/recipes.js
@@ -1,11 +1,9 @@
 if (global.hasAllMods(['createadditionallogistics', 'create_connected'])) {
   ServerEvents.recipes((event) => {
-    if (true) {
-      event.replaceInput(
-        { output: 'createadditionallogistics:flexible_shaft' },
-        'create:brass_casing',
-        'create_connected:brass_gearbox'
-      );
-    }
+    event.replaceInput(
+      { output: 'createadditionallogistics:flexible_shaft' },
+      'create:brass_casing',
+      'create_connected:brass_gearbox'
+    );
   });
 }
